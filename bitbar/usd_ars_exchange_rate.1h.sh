8f0a742af1829d539669899a38b042c13eafd9b3
Haochi Chen <github@ihaochi.com>
2016-12-10 21:07:22 -0300

    Save some space

--- bitbar/usd_ars_exchange_rate.1h.sh
+++ bitbar/usd_ars_exchange_rate.1h.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:/usr/local/bin
 
 app_id=$(jq --raw-output ".openexchangerates.app_id" ~/.bitbar)
 exchange_rate=$(curl -s "https://openexchangerates.org/api/latest.json?app_id=$app_id&base=USD" | jq ".rates.ARS")
-printf 'USD:ARS %.*f\n' 2 "$exchange_rate"
+printf '%.*f\n' 2 "$exchange_rate"
